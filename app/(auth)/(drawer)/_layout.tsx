d728e9f419641049bac40e351cb5d9db30b0ff3b
Erhan İkinci <e.ikinci.reismakina@gmail.com>
2024-06-02 11:39:26 +0300

    publishable key added

---
+++ app/(auth)/(drawer)/_layout.tsx
@@ -0,0 +1,187 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  useWindowDimensions,
+  TextInput,
+} from "react-native";
+import React from "react";
+import { Drawer } from "expo-router/drawer";
+import { Link, useNavigation } from "expo-router";
+import { FontAwesome6, Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import { DrawerActions } from "@react-navigation/native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+
+export const CustomDrawerContent = (props: any) => {
+  const { bottom, top } = useSafeAreaInsets();
+
+  return (
+    <View style={{ flex: 1, marginTop: top }}>
+      <View style={{ backgroundColor: "#fff", paddingBottom: 16 }}>
+        <View style={styles.searchSection}>
+          <Ionicons
+            style={styles.searchIcon}
+            name="search"
+            size={20}
+            color={Colors.grey}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Search"
+            underlineColorAndroid={"transparent"}
+          />
+        </View>
+        <Text>Header</Text>
+      </View>
+
+      <DrawerContentScrollView
+        contentContainerStyle={{ paddingTop: 0 }}
+        {...props}
+      >
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+    </View>
+  );
+};
+
+const Layout = () => {
+  const navigation = useNavigation();
+  const dimentions = useWindowDimensions();
+  return (
+    <Drawer
+      drawerContent={CustomDrawerContent}
+      screenOptions={{
+        headerLeft: () => (
+          <TouchableOpacity
+            onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}
+            style={{ marginLeft: 16 }}
+          >
+            <FontAwesome6 name="grip-lines" size={28} color={Colors.grey} />
+          </TouchableOpacity>
+        ),
+        headerStyle: {
+          backgroundColor: Colors.light,
+        },
+        headerShadowVisible: false,
+        drawerActiveBackgroundColor: Colors.selected,
+        drawerActiveTintColor: "#000",
+        drawerInactiveTintColor: "#000",
+        drawerItemStyle: { borderRadius: 12 },
+        drawerLabelStyle: { marginLeft: -20 },
+        overlayColor: "rgba(0, 0, 0, 0.2)",
+        drawerStyle: { width: dimentions.width * 0.86 },
+      }}
+    >
+      <Drawer.Screen
+        name="(chat)/new"
+        getId={() => Math.random().toString()}
+        options={{
+          title: "ChatGPT",
+          drawerIcon: () => (
+            <View style={[styles.item, { backgroundColor: "#000" }]}>
+              <Image
+                source={require("@/assets/images/logo-white.png")}
+                style={styles.btnImage}
+              />
+            </View>
+          ),
+          headerRight: () => (
+            <Link href="/(auth)/(drawer)/(chat)/new" push asChild>
+              <TouchableOpacity>
+                <Ionicons
+                  name="create-outline"
+                  size={24}
+                  color={Colors.grey}
+                  style={{ marginRight: 16 }}
+                />
+              </TouchableOpacity>
+            </Link>
+          ),
+        }}
+      />
+
+      <Drawer.Screen
+        name="dalle"
+        options={{
+          title: "Dall-E",
+          drawerIcon: () => (
+            <View style={[styles.item, { backgroundColor: "#000" }]}>
+              <Image
+                source={require("@/assets/images/dalle.png")}
+                style={styles.dallEImage}
+              />
+            </View>
+          ),
+        }}
+      />
+
+      <Drawer.Screen
+        name="explore"
+        options={{
+          title: "Explore GPTs",
+          drawerIcon: () => (
+            <View style={styles.exploreItem}>
+              <Ionicons name="apps-outline" size={18} color={"#000"} />
+            </View>
+          ),
+        }}
+      />
+    </Drawer>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    borderRadius: 15,
+    overflow: "hidden",
+  },
+  btnImage: {
+    margin: 6,
+    width: 16,
+    height: 16,
+  },
+  dallEImage: {
+    width: 28,
+    height: 28,
+    resizeMode: "cover",
+  },
+  exploreItem: {
+    borderRadius: 15,
+    backgroundColor: "#fff",
+    width: 28,
+    height: 28,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  searchSection: {
+    marginHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.input,
+    borderRadius: 10,
+    height: 10,
+    width: 34,
+  },
+  searchIcon: {
+    padding: 6,
+  },
+  input: {
+    flex: 1,
+    paddingTop: 8,
+    paddingRight: 8,
+    paddingBottom: 8,
+    paddingLeft: 0,
+    alignItems: "center",
+    color: '#424242',
+  },
+});
+
+export default Layout;
